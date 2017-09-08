e0879ddc3b4ee3c8827d33cd4a9afc42ae61ac3d
Prem Prakash <prem1014prakash@gmail.com>
2017-09-08 08:16:02 +0530

    'committed'

--- nrf/src/app/donate/donate.component.ts
+++ nrf/src/app/donate/donate.component.ts
@@ -5,11 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './donate.component.html',
   styleUrls: ['./donate.component.css']
 })
-export class DonateComponent implements OnInit {
 
+export class DonateComponent implements OnInit {
+donation: Donation = {
+  amount: 200,
+  name: 'Prem Prakash',
+  email: 'prem1014prakash@gmail.com'
+};
   constructor() { }
 
   ngOnInit() {
   }
+saveData():void{
+alert()
+}
+}
 
+export class Donation {
+  amount: number;
+  name: string;
+  email: string;
 }
